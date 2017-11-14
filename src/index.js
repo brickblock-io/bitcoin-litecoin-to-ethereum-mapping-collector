a27710000a1f8ad3c1b110f1e42a518419def177
Martin Josefsson <martin.josefsson@brickblock.io>
2017-11-14 15:13:42 +0100

    Readiness probe fix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ app.get("/address-map", (req, res) =>
   res.send(`it works! req.message: ${req.message}`)
 )
 
+app.get("/", (req, res) => res.send("hello world"))
+
 const port = 8080
 app.listen(port, () =>
   console.log(`App running on port ${port}. Check /address-map`)
