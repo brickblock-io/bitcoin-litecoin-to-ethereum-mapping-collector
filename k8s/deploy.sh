d38b8705cf8fc72952bb2d7a365c4007361648e7
andrew.holway <andrew.holway@brickblock.io>
2017-11-22 15:25:20 +0100

    removed env var check

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -3,27 +3,10 @@
 set -eufx -o pipefail
 
 export KUBECONFIG="$(pwd)/kubeconfig"
+export KUBE_NAMESPACE=$1
 
 # Enable Globbing
 shopt -s extglob
-
-# Do we have all the variables we need?
-: "${debug:?Environment variable debug needs to be set before running this script}"
-: "${NAME:?You need to pass the name of the service (e.g. 'platform-frontend') as an argument to the deploy script (e.g. ./deploy.sh platform-frontend)}"
-: "${GITLAB_USER_EMAIL:?Environment variable GITLAB_USER_EMAIL needs to be set before running this script}"
-: "${CI_REGISTRY:?Environment variable CI_REGISTRY needs to be set before running this script}"
-: "${CI_REGISTRY_USER:?Environment variable CI_REGISTRY_USER needs to be set before running this script}"
-: "${CI_REGISTRY_PASSWORD:?Environment variable CI_REGISTRY_PASSWORD needs to be set before running this script}"
-: "${CI_REGISTRY_IMAGE:?Environment variable CI_REGISTRY_IMAGE needs to be set before running this script}"
-#: "${CI_REGISTRY_TAG:?Environment variable CI_REGISTRY_TAG needs to be set before running this script}"
-: "${CI_ENVIRONMENT_SLUG:?Environment variable CI_ENVIRONMENT_SLUG needs to be set before running this script}"
-: "${CI_ENVIRONMENT_URL:?Environment variable CI_ENVIRONMENT_URL needs to be set before running this script}"
-: "${CI_ENVIRONMENT_HOSTNAME:?Environment variable CI_ENVIRONMENT_HOSTNAME needs to be set before running this script}"
-: "${CI_PIPELINE_ID:?Environment variable CI_PIPELINE_ID needs to be set before running this script}"
-: "${CI_JOB_ID:?Environment variable CI_JOB_ID needs to be set before running this script}"
-: "${CI_COMMIT_REF_NAME:?Environment variable CI_COMMIT_REF_NAME needs to be set before running this script}"
-: "${KUBE_NAMESPACE:?Environment variable KUBE_NAMESPACE needs to be set before running this script}"
-: "${KUBE_URL:?Environment variable KUBE_URL needs to be set before running this script}"
 
 # Write out the certificate for the K8s API
 export KUBE_CLUSTER_OPTIONS=
