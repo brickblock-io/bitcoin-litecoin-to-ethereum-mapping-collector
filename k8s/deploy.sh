ce7fa2ef1e92f4930c699b1661fb38ee5849d74f
andrew.holway <andrew.holway@brickblock.io>
2017-11-22 15:06:05 +0100

    broken ymls

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euf -o pipefail
+set -eufx -o pipefail
 
 export KUBECONFIG="$(pwd)/kubeconfig"
 
@@ -35,10 +35,11 @@ kubectl config use-context gitlab-deploy
 # Its not part of standard tools so its use might be considered a bit risque in certain circles.
 
 # Deployment
-cat $(pwd)/k8s/yml/deployment.yml | tee $(tty) | envsubst | kubectl apply -n $KUBE_NAMESPACE -f - --insecure-skip-tls-verify=true
+cat $(pwd)/k8s/yml/deployment.yml | envsubst
+cat $(pwd)/k8s/yml/deployment.yml | envsubst | kubectl apply -n $KUBE_NAMESPACE -f - --insecure-skip-tls-verify=true
 
 # Service
-cat $(pwd)/k8s/yml/service.yml | tee $(tty) | envsubst | kubectl apply -n $KUBE_NAMESPACE -f - --insecure-skip-tls-verify=true
+cat $(pwd)/k8s/yml/service.yml | envsubst | kubectl apply -n $KUBE_NAMESPACE -f - --insecure-skip-tls-verify=true
 
 # Ingress
-cat $(pwd)/k8s/yml/ingress.yml | tee $(tty) | envsubst | kubectl apply -n $KUBE_NAMESPACE -f - --insecure-skip-tls-verify=true
+cat $(pwd)/k8s/yml/ingress.yml | envsubst | kubectl apply -n $KUBE_NAMESPACE -f - --insecure-skip-tls-verify=true
