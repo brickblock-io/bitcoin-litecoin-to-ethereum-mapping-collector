1bf4627a93e4b7f2e383d42835f4d56f437c24c6
mattgstevens <mattgstevens@gmail.com>
2017-11-24 19:20:25 +0100

    fix: catch errors that the db connection may throw, use a connection pool

--- migrations/1511529171-UP-create-table-address-mapping.sql
+++ migrations/1511529171-UP-create-table-address-mapping.sql
@@ -1,10 +1,11 @@
 -- 1511529171 UP create-table-address-mapping
 
 CREATE TABLE IF NOT EXISTS address_mapping(
-       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-       createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-       updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-       address TEXT,
-       ethereumAddress TEXT,
-       signature TEXT
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  address TEXT,
+  ethereumAddress TEXT,
+  signature TEXT,
+  UNIQUE KEY signature (signature(100))
 );
